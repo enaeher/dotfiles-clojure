dd774b652640ced53ee5bb434ae2125cece4cda8
Eli Naeher <eli@outpace.com>
2014-11-26 18:51:23 -0600

    Update nrepl version, add refactor-nrepl and humane-test-output

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]]}}
+{:user
+ {:dependencies [[pjstadig/humane-test-output "0.6.0"]]
+  :injections [(require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)]
+  :plugins [[cider/cider-nrepl "0.8.2-SNAPSHOT"]
+            [refactor-nrepl "0.1.0"]]}}
