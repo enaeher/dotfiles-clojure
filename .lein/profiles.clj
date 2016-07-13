a233b283b41f277217c22ef7178f31cfe53fc7ea
Eli Naeher <eli@naeher.name>
2016-07-13 09:56:41 -0400

    Update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,26 +1,27 @@
-{:user
+{:repl
+ {:dependencies [;; [acyclic/squiggly-clojure "0.1.5"]
+                 [org.clojure/tools.nrepl "0.2.12"]
+                 [enaeher/contrail "0.2.0"]]
+  :plugins [[cider/cider-nrepl "0.11.0"]
+            [thunknyc/nrepl-profile "0.1.0-SNAPSHOT"]
+            [refactor-nrepl "2.2.0-SNAPSHOT"]
+            [lein-kibit "0.1.2"]
+            [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]
+  ;; :env {:squiggly {:checkers [:kibit]}}
+  }
+ :user
  {:dependencies [[pjstadig/humane-test-output "0.6.0"]
                  [io.aviso/pretty "0.1.8"]
-                 [alembic "0.3.2"]
-                 [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]
-                 [org.clojure/tools.nrepl "0.2.7"]
-                 [enaeher/contrail "0.2.0"]
                  [slamhound "1.5.5"]]
-  :injections [(require 'pjstadig.humane-test-output)
-               (pjstadig.humane-test-output/activate!)]
-  :plugins [[cider/cider-nrepl "0.9.0"]
-            [refactor-nrepl "1.1.0-SNAPSHOT"]
-            [lein-kibit "0.0.8"]
-            [lein-environ "1.0.0"]
-            [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
-            [thunknyc/nrepl-profile "0.1.0-SNAPSHOT"]
-            [com.palletops/lein-shorthand "0.4.0"]
-            [lein-ancient "0.6.7"]]
   :shorthand {e [clojure.pprint/pprint
                  contrail.core/trace
                  contrail.core/untrace
                  contrail.core/all-traced
                  contrail.core/traced?]}
+  :injections [(require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)]
+  :plugins [[com.jakemccrary/lein-test-refresh "0.11.0"]
+            [lein-ancient "0.6.7"]
+            [com.palletops/lein-shorthand "0.4.0"]]
   :jvm-opts ["-XX:MaxJavaStackTraceDepth=100000" "-XX:-OmitStackTraceInFastThrow"]
-  :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-  :env {:squiggly {:checkers [:kibit]}}}}
+  :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
